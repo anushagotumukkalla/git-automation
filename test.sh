4858634c4b96d5bdc62b831f17695ed557170b04
Anusha Varma <anushavarma@anushas-mbp.lan>
2019-06-17 22:18:54 -0500

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-//git Download file from git.
+pwd
+curl -H 'Authorization: token 1a76cf588eea42bc27a7f38391d2220dc1da5512 ' -H 'Accept: application/vnd.github.v3.raw' -o test1.sh https://raw.githubusercontent.com/anushagotumukkalla/git-automation/master/test.sh
+
