3c3c5f51f664f70e2868b24af57d79875d55f23f
Anusha Varma <anushavarma@anushas-mbp.lan>
2019-06-17 22:26:12 -0500

    inventory

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 pwd
-curl -H 'Authorization: token 1a76cf588eea42bc27a7f38391d2220dc1da5512 ' -H 'Accept: application/vnd.github.v3.raw' -o test1.sh https://raw.githubusercontent.com/anushagotumukkalla/git-automation/master/test.sh
-
+curl -H 'Authorization: token 1a76cf588eea42bc27a7f38391d2220dc1da5512 ' -H 'Accept: application/vnd.github.v3.raw' -o inventory.sh https://raw.githubusercontent.com/anushagotumukkalla/git-automation/master/inventory.sh
+ 
+ cp -f inventory.sh /Users/anushavarma/Desktop/testing
